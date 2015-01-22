b3c5fdb2a3ad77b04dc058233f0fc0f87dd4405b
kissyteam <tiehang.lth@taobao.com>
2015-01-22 16:08:06 +0800

    sync daily version 1.0.3

--- 1.0.3/build/tpl-render.js
+++ 1.0.3/build/tpl-render.js
@@ -1,4 +1,4 @@
-KISSY.add('kg/vc-pagination/1.0.3/tpl-render',["./tpl","kg/xtemplate/3.3.3/runtime"],function(S ,require, exports, module) {
+KISSY.add('kg/vc-pagination/1.1.0/tpl-render',["./tpl","kg/xtemplate/3.3.3/runtime"],function(S ,require, exports, module) {
 
 
 var tpl = require("./tpl");
